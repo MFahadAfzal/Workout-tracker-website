f45a587ec4e3c4a8dfec0e6a09da6a6098bc289b
MFahadAfzal <fahad.afzal780@gmail.com>
2026-04-22 21:51:43 -0600

    added login page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,7 @@ export default function Navbar() {
     <nav className="flex justify-between items-center bg-gray-900 px-8 py-4">
       <h1 className="text-white font-bold text-xl">Workout Tracker</h1>
       <div className="flex gap-8">
+        <Link href="/login" className="text-gray-300 hover:text-white">Login</Link>
         <Link href="/" className="text-gray-300 hover:text-white">Home</Link>
         <Link href="/workout" className="text-gray-300 hover:text-white">Log Workout</Link>
         <Link href="/history" className="text-gray-300 hover:text-white">History</Link>
