eb3467ebff8b64e54dcacd84805a57507d331c2c
MFahadAfzal <fahad.afzal780@gmail.com>
2026-04-22 21:40:59 -0600

    style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="flex gap-4 bg-gray-900 p-4">
       <Link href="/">Home</Link>
       <Link href="/workout">Log Workout</Link>
       <Link href="/history">History</Link>
