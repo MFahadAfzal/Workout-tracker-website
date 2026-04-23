008dad61f307efd6147083480e25e919cc43ec64
MFahadAfzal <fahad.afzal780@gmail.com>
2026-04-22 21:42:22 -0600

    added styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,13 @@ import Link from "next/link"
 
 export default function Navbar() {
   return (
-    <nav className="flex gap-4 bg-gray-900 p-4">
-      <Link href="/">Home</Link>
-      <Link href="/workout">Log Workout</Link>
-      <Link href="/history">History</Link>
+    <nav className="flex justify-between items-center bg-gray-900 px-8 py-4">
+      <h1 className="text-white font-bold text-xl">Workout Tracker</h1>
+      <div className="flex gap-8">
+        <Link href="/" className="text-gray-300 hover:text-white">Home</Link>
+        <Link href="/workout" className="text-gray-300 hover:text-white">Log Workout</Link>
+        <Link href="/history" className="text-gray-300 hover:text-white">History</Link>
+      </div>
     </nav>
   )
 }
