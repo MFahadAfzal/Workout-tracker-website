b1ca97ab1380eda8afc963b0cebeca630774f894
MFahadAfzal <fahad.afzal780@gmail.com>
2026-04-27 17:16:09 -0600

    testing

--- utils/db/db.server.ts
+++ utils/db/db.server.ts
@@ -4,4 +4,5 @@ export async function getWorkouts(){
     const supabase = await createClient()
     const {data:{user}} = await supabase.auth.getUser()
     const { data } = await supabase.from('workouts').select().eq('user_id', user!.id)
+    return data
 }
