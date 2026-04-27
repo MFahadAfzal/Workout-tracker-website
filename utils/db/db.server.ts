46d2f095f41cf0b12d9355ffe85bacd2cadf9063
MFahadAfzal <fahad.afzal780@gmail.com>
2026-04-27 17:15:16 -0600

    testing

--- utils/db/db.server.ts
+++ utils/db/db.server.ts
@@ -3,4 +3,5 @@ import { createClient } from "../supabase/server";
 export async function getWorkouts(){
     const supabase = await createClient()
     const {data:{user}} = await supabase.auth.getUser()
+    const { data } = await supabase.from('workouts').select().eq('user_id', user!.id)
 }
